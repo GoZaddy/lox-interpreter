80b464043ea0c7bca8724065096567d7dd3a3378
GoZaddy <faruqyusuff437@gmail.com>
2023-10-18 00:57:11 -0500

    updates

--- scanner.cpp
+++ scanner.cpp
@@ -9,7 +9,7 @@ using namespace std;
 class Scanner {
     private:
         string source;
-        vector<string> tokens;
+        vector<Token> tokens;
         int start = 0;
         int current = 0;
         int line = 1;
@@ -18,14 +18,136 @@ class Scanner {
             return current >= source.length();
         }
 
+        char advance() {
+            return source.at(current++);
+        }
+
+        void addToken(TokenType type) {
+            addToken(type, NULL);
+        }
+
+        void addToken(TokenType type, string literal) {
+            string text = source.substr(start, current);
+            tokens.push_back(Token(type, text, literal, line));
+        }
+
+        bool match(char expected) {
+            if (isAtEnd()) {return false;}
+            if (source.at(current) != expected) {return false;}
+
+            current++;
+            return true;
+        }
+
+        char peek() {
+            if (isAtEnd()) return '\0';
+            return source.at(current);
+        }
+
+        char peekNext() {
+            if (current + 1 >= source.length()) return '\0';
+            return source.at(current + 1);
+        }
+
+        void string() {
+            while (peek() != '"' && !isAtEnd()) {
+                if (peek() == '\n') line++;
+                advance();
+            }
+
+            if (isAtEnd()) {
+                // Lox.error(line, "Unterminated string.");
+                return;
+            }
+
+            // The closing ".
+            advance();
+
+            // Trim the surrounding quotes.
+            string val = source.substr(start + 1, current - 1);
+            addToken(STRING, val);
+        }
+
+        void number(){
+            while (isdigit(peek())) {advance()};
+
+            // Look for a fractional part.
+            if (peek() == '.' && isdigit(peekNext())) {
+            // Consume the "."
+            advance();
+
+                while (isdigit(peek())) advance();
+            }
+
+            addToken(
+                NUMBER,
+                stod(source.substr(start, current))
+            );
+        }
+
         void scanToken(){
-            
+            char c = advance();
+            switch (c) {
+                case '(': addToken(LEFT_PAREN); break;
+                case ')': addToken(RIGHT_PAREN); break;
+                case '{': addToken(LEFT_BRACE); break;
+                case '}': addToken(RIGHT_BRACE); break;
+                case ',': addToken(COMMA); break;
+                case '.': addToken(DOT); break;
+                case '-': addToken(MINUS); break;
+                case '+': addToken(PLUS); break;
+                case ';': addToken(SEMICOLON); break;
+                case '*': addToken(STAR); break; 
+                case '!':
+                    addToken(match('=') ? BANG_EQUAL : BANG);
+                    break;
+                case '=':
+                    addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                    break;
+                case '<':
+                    addToken(match('=') ? LESS_EQUAL : LESS);
+                    break;
+                case '>':
+                    addToken(match('=') ? GREATER_EQUAL : GREATER);
+                    break;
+                case '/':
+                    if (match('/')) {
+                        // A comment goes until the end of the line.
+                        while (peek() != '\n' && !isAtEnd()){ 
+                            advance();
+                        }
+                    } else {
+                        addToken(SLASH);
+                    }
+                    break;
+
+                case ' ':
+                case '\r':
+                case '\t':
+                    // Ignore whitespace.
+                    break;
+
+                case '\n':
+                    line++;
+                    break;
+                case '"': 
+                    string(); 
+                    break;
+                default:
+                    if (isdigit(c)){
+                        number();
+                    } else {
+                        // Lox.error(line, "Unexpected character."); // todo: fix this. probably put error() and repor() in another file and import here
+
+                    }
+                    break;
+            }
         }
 
     public:
-        Scanner(string source){
+        Scanner (string source) {
             this->source = source;
-        }
+        };
 
         vector<Token> scanTokens() {
             while (!isAtEnd()) {
@@ -34,7 +156,7 @@ class Scanner {
             scanToken();
             }
 
-            tokens.push_back(Token(EOF, "", null, line));
+            tokens.push_back(Token(END_OF_FILE, "", NULL, line));
             return tokens;
-        }
+        };
 };
--- token.cpp
+++ token.cpp
@@ -2,16 +2,18 @@
 
 #include "token.h"
 
+
 Token::Token(TokenType type, string lexeme, string literal, int line) {
     this->type = type;
     this->lexeme = lexeme;
     this->literal = literal;
     this->line = line;
 };
-
+ 
 string Token::toString(){
     stringstream ss;
 
-    ss << this->type << " " << this->lexeme << " " << this->literal << endl; 
+    ss << this->type << " " << this->lexeme << " " << this->literal << endl; // fix this
+    
     return ss.str();
 };
