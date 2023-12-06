33f3ed25f4675e041bde0e0ad58ac3a2b8298712
Faruq Yusuff <fgyusuff@crimson.ua.edu>
2023-12-06 15:36:01 -0600

    updates

---
+++ compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ -w -std=c++11 main.cpp scanner.cpp token.cpp util.cpp parser.cpp expr.cpp stmt.cpp interpreter.cpp return.cpp lox_function.cpp native_funcs.cpp environment.cpp resolver.cpp lox_class.cpp lox_instance.cpp
